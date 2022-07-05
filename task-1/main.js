4791e0f0fa7cf79abde366309046821fb2d4a71a
nesrinmirza <106273121+nesrinmirza@users.noreply.github.com>
2022-07-05 09:21:38 -0700

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,8 +1,18 @@
-// Use this matrix for solution
 let matrix = [
     [4, -2, 5],
     [1, -4, -12],
     [0, 1, -3]
 ]
+let mat2= 0;
+let count = 0;
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    if (matrix[i][j]<0 && matrix[i][j]%2==0) {
+        mat2 = mat2 +matrix[i][j] 
+        count++;
+    }
 
-// Write code here. 
+  }
+}
+console.log(mat2);
+console.log(count);
